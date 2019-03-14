cd1b3e7aad3eccf082d95a11d6e2c7d8e1eadd67
moiekodom <48547419+moiekodom@users.noreply.github.com>
2019-03-14 12:02:28 +0300

    Add files via upload

---
+++ src/platform/ZWaySerialController.cpp
@@ -0,0 +1,47 @@
+#include "ZWaySerialController.h"
+
+namespace OpenZWaveMe
+{
+    ZWaySerialController::ZWaySerialController() :
+        m_lastError(NoError)
+    {
+        memset(&m_zway, 0, sizeof(ZWay));
+    }
+    //----------------------------------------------------------------------------------------------------
+
+    ZWaySerialController::~ZWaySerialController()
+    {
+    }
+    //----------------------------------------------------------------------------------------------------
+
+    bool ZWaySerialController::Open(string const& _serialControllerName)
+    {
+        m_lastError = zway_init(&m_zway, _serialControllerName.c_str(), NULL, NULL, NULL, NULL, NULL);
+
+        if (m_lastError != NoError)
+        {
+            OZW_FATAL_ERROR(InvalidPort, "Failed to init ZWay");
+
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+    //----------------------------------------------------------------------------------------------------
+
+    bool ZWaySerialController::Close()
+    {
+        m_lastError = zway_stop(m_zway);
+
+        zway_terminate(&m_zway);
+
+        return !(bool)m_lastError;
+    }
+    //----------------------------------------------------------------------------------------------------
+
+    uint32 ZWaySerialController::Write(uint8* _buffer, uint32 _length)
+    {
+        return 0;
+    }
+    //----------------------------------------------------------------------------------------------------
+}
---
+++ src/platform/ZWaySerialController.h
@@ -0,0 +1,30 @@
+#ifndef SERIALCONTROLLER_H_INCLUDED
+#define SERIALCONTROLLER_H_INCLUDED
+
+#include <ZWayLib.h>
+
+#include <platform/Controller.h>
+
+#include "../Defs.h"
+
+using OpenZWave::Controller;
+
+namespace OpenZWaveMe
+{
+    class ZWaySerialController : public Controller
+    {
+        private:
+            ZWError m_lastError;
+            ZWay m_zway;
+
+        public:
+            ZWaySerialController();
+            virtual ~ZWaySerialController();
+
+            bool Open(string const& _serialControllerName);
+            bool Close();
+            uint32 Write(uint8* _buffer, uint32 _length);
+    };
+}
+
+#endif // SERIALCONTROLLER_H_INCLUDED
