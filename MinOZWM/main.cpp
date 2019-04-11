f1e825b16b8836bd5a036825c428e9e93412c535
Дмитрий Косарев <moiekodom@gmail.com>
2019-04-11 13:23:27 +0300

    Commit #3

--- MinOZWM/main.cpp
+++ MinOZWM/main.cpp
@@ -1,15 +1,19 @@
-#include <ZWayLib.h>
+//#include <ZWayLib.h>
 
-using namespace std;
+#include "../src/Manager.h"
+
+using namespace OpenZWaveMe;
 
 int main()
 {
-    ZWay zway       = NULL;
+    /*ZWay zway       = NULL;
     ZWLog logger    = zlog_create(stdout, Debug);
 
     ZWError r = zway_init(&zway, ZSTR("/dev/ttyACM0"), NULL, NULL, NULL, NULL, logger);
 
-    printf("Hello World!\n");
+    printf("Hello World!\n");*/
+
+    Manager::Create();
 
     return 0;
 }
